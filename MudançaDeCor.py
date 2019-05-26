474217a42f3116de080f61550f8d1b2f860dcdea
Iago Assis Barreto <barretoprofissional@gmail.com>
2019-05-26 20:47:46 -0300

    .

--- MudançaDeCor.py
+++ MudançaDeCor.py
@@ -1,6 +1,10 @@
 import cv2
 import numpy as np
-img = cv2.imread('t.jpg')
+#no canal  HSV  hue : 0  - 180 , Saturation : 0 -255 , value : 255
 
+img  =  cv2.imread('t.jpg')
 
-cv2.imshow('nomeDaTela',img)
+img_HSV = cv2.cvtcolor(img, cv2.COLOR_BGR2HSV)
+
+cv2.imshow('imagem HSV',img_HSV)
+cv2.imshow('HUE ',img_HSV, img[ : , : , 0])
