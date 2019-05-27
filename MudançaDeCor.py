bc747b9dc92be3ba0301e811cfe17d03399e30ce
Iago Assis Barreto <barretoprofissional@gmail.com>
2019-05-26 21:22:37 -0300

    Convertendo RGB para HSV
    além disso salvando as imagens de cada canal em um arquivo diferente

--- MudançaDeCor.py
+++ MudançaDeCor.py
@@ -1,10 +1,24 @@
 import cv2
-import numpy as np
 #no canal  HSV  hue : 0  - 180 , Saturation : 0 -255 , value : 255
 
 img  =  cv2.imread('t.jpg')
 
-img_HSV = cv2.cvtcolor(img, cv2.COLOR_BGR2HSV)
-
+img_HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+TEMP4 = img_HSV
 cv2.imshow('imagem HSV',img_HSV)
-cv2.imshow('HUE ',img_HSV, img[ : , : , 0])
+
+cv2.imshow('HUE ', img_HSV[ : , : , 0])
+TEMP1 = img_HSV[ : , : , 0]
+cv2.imshow('SATURATION', img_HSV[ : , : , 1])
+TEMP2 = img_HSV[ : , : , 1]
+cv2.imshow('VALUE CHANNEL', img_HSV [ : , : , 2])
+TEMP3 = img_HSV[ : , : , 2]
+
+cv2.imwrite("HUE.png", TEMP1)
+cv2.imwrite("SATURATION.png", TEMP2)
+cv2.imwrite("VALUE CHANNEL.png", TEMP3)
+cv2.imwrite("HSV.png", TEMP4)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
