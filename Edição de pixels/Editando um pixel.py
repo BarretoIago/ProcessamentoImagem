937e4bb00c903141f752b4fb534cb84eff507c16
Iago Assis Barreto <barretoprofissional@gmail.com>
2019-07-16 01:34:21 -0300

    minor error fix

--- Edição de pixels/Editando um pixel.py
+++ Edição de pixels/Editando um pixel.py
@@ -2,17 +2,24 @@ import cv2
 import numpy as np
 ##vamos ler nossa querida lena, e vamos selecionar um pixel de sua imagem.
 img = cv2.imread('lena_std.tif')
+numR= input("Digite um número para o tom RED:")
+numG= input("Digite um número para o tom GREEN:")
+numB= input("Digite um número para o tom BLUE:")
+def py as int
+def px as int
+
+py=input("Digite  a cordenada Y do pixel:")
+px=input("Digite  a cordenada X do pixel:")
+##vamos tentar receber os dados do usuário
+
+print("Cor original do pixel")
+print (img['py,px'])
+img[py,px]= [numR, numG, numB]
+cv2. imshow('lena_std.tif',img)
+
 ##mostramos a lena, e então usamos uma váriavel para receber as cordenadas de linha e coluna
 ##e então podemos imprimir o valor RGB desse pixel.
-cv2. imshow('lena_std.tif',img)
-pix = img[500,510]
+
+pix = img[py,px]
+print("Nova cor do pixel")
 print (pix)
-
-
-##No codigo abaixo, é modificado o pixel 1-1 da matriz da imagem para o valor
-##RGB 43, 239, 102, verde
-## para podermos encontra-lo facilmente.
-## e depois imprime a imagem, 
-img[0,0]= [43, 239, 102]
-print (img[1,1])
-cv2. imshow('lena_std.tif',img)
